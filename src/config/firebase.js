2b2a845ae7ea2a17835fb001b3a8de6f235e8ade
nguyen.tien.dat <nguyen.tien.dat@orlab.co.jp>
2024-01-30 09:42:58 +0700

    update file env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCg1h6a1UN-Erw60zPu_7DJp0kLFqO6FZE",
-  authDomain: "orlvn-2023.firebaseapp.com",
-  databaseURL: "https://orlvn-2023-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "orlvn-2023",
-  storageBucket: "orlvn-2023.appspot.com",
-  messagingSenderId: "362808581261",
-  appId: "1:362808581261:web:dd29035de0a17c2a80d6c4",
-  measurementId: "G-3SXXLD0FXP"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 
